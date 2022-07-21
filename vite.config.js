2d0ff47b9fe71556c6b3a001caa2bd8a26248b83
yong <yong@asial.co.jp>
2022-07-21 16:41:11 +0900

    feat: update built files, gitignore, and vite configuration

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default {
   build: {
     outDir: BUILD_DIR,
     assetsInlineLimit: 0,
-    emptyOutDir: true,
+    emptyOutDir: false,
     rollupOptions: {
       output: {
         entryFileNames: `assets/[name].js`,
